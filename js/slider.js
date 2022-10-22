341ede5724ce79b4b969f7753991cbe7b620c870
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-10-22 16:38:36 +0300

    :)

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,59 @@
 $(document).ready(function (){
-    $('.slider').slick();
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: "ease",
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: false
+                }
+            }
+        ]
+    });
+    $('.slider2').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: "ease",
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive:[
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: false
+                }
+            }
+        ]
+    });
 })
